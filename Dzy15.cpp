a349ea61b5c83f6b112aac33875099514da8737f
Harshchopade <harshvardhanchopade723@gmail.com>
2025-08-29 19:11:21 +0530

    Create Dzy15.cpp
    Day15

---
+++ Dzy15.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+    vector<int> lastIndex(256, -1);
+    int left = 0, maxLen = 0;
+
+    for (int right = 0; right < s.size(); right++) {
+        char c = s[right];
+        if (lastIndex[c] >= left) {
+            left = lastIndex[c] + 1;
+        }
+        lastIndex[c] = right;
+        maxLen = max(maxLen, right - left + 1);
+    }
+    return maxLen;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << lengthOfLongestSubstring(s) << endl;
+    return 0;
+}
