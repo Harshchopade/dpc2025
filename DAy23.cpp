c0896c2c615032c0ab90896ddd99fc8adb572497
Harshchopade <harshvardhanchopade723@gmail.com>
2025-09-06 22:45:13 +0530

    Create DAy23.cpp
    Day23

---
+++ DAy23.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <deque>
+#include <vector>
+using namespace std;
+
+vector<int> slidingWindowMaximum(vector<int>& arr, int k) {
+    deque<int> dq;
+    vector<int> result;
+    int n = arr.size();
+
+    for (int i = 0; i < n; i++) {
+        if (!dq.empty() && dq.front() <= i - k) dq.pop_front();
+        while (!dq.empty() && arr[dq.back()] <= arr[i]) dq.pop_back();
+        dq.push_back(i);
+        if (i >= k - 1) result.push_back(arr[dq.front()]);
+    }
+    return result;
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    vector<int> ans = slidingWindowMaximum(arr, k);
+    for (int x : ans) cout << x << " ";
+    return 0;
+}
